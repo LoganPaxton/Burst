a6b6ce3dfa8520dc4274931b25706e48c5aec537
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-07-24 11:41:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
 # CONFIGURATION OPTIONS #
 # FILE_PATH="tests/basic.br" Currently does nothing (Coming in a future update!)
 # COMPILER="node.js" Currently does nothing (Coming in a future update!)
 
-node src/burst.js
+if [ -z "$1" ]; then
+  echo "Usage: $0 <file_path>"
+  exit 1
+fi
+
+FILE_PATH=$1
+
+node src/burst.js "$FILE_PATH"
